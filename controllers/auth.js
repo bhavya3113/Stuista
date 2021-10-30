9b97dc955cb2c33fba323d5b52c4b37cc99c339d
january31 <agrawalbhavya3113@gmail.com>
2021-10-30 19:29:39 +0530

    sending otp for verification

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,22 @@
 const bcrypt = require("bcryptjs");
 const {validationResult} = require("express-validator");
+const nodemailer = require("nodemailer");
+const sendGridTransport = require("nodemailer-sendgrid-transport");
+const dotenv = require("dotenv");
+const otpGenerator = require("otp-generator");
+const jwt = require("jsonwebtoken");
+
+
+dotenv.config();
 
 const User = require("../models/users");
+const Otp = require("../models/otp");
 
+const transporter = nodemailer.createTransport(sendGridTransport({
+  auth:{
+    api_key: process.env.API_KEY
+  }
+}))
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -16,6 +30,10 @@ const firstname = req.body.firstname;
 const lastname = req.body.lastname;
 const email = req.body.email;
 const password = req.body.password;
+
+if (!(email && password && firstname && lastname)) {
+  res.status(400).send("All input is required");
+}
 
 User.findOne({email:email})
 .then(user=>{
@@ -36,16 +54,45 @@ bcrypt.hash(password, 12)
     email: email,
     password: hashedPassword
   });
- return  user.save();
+   user.save();
 })
 .then(results=>{
-  res.status(201).json({message:'user created', email:email,firstname:firstname});
-  console.log(results);
+  
+  // res.status(201).json({message:'user created', email:email,firstname:firstname});
+  let otp = otpGenerator.generate(6, {
+    alphabets: false,
+    specialChars: false,
+    upperCase: false,
+  });
+  const onetimepwd = new Otp({
+    email:email,
+    firstname:firstname,
+    otp:otp
+  });
+   onetimepwd.save();
+   res.status(200).json({
+    message: "otp sent",
+    email: email,
+  });
+  return transporter.sendMail({
+    to:email,
+    from:'learnatstuista@gmail.com',
+    subject:'Verification OTP',
+    html:`<h4>Hello ${firstname},</h4>
+    <br>Please use this One time password to verify your account.<br>
+    OTP:${otp}<br>
+    Do not share it with anyone.<br>
+    <h5>Thanks ,<br>Team Stuista</h5>`
+  })
 })
 .catch(err => {
   if (!err.statusCode) {
     err.statusCode = 500;
-    console.log(err);
+    console.log("email not send",err);
   }
 });
+}
+
+exports.otpVerification =(req,res,next)=>{
+
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const {body} = require("express-validator");
 
 const user = require("../models/users");
 const authController = require("../controllers/auth");
 
 router.post("/signup", authController.signup);
-
+router.post("/signup/verifyotp",authController.otpVerification);
 module.exports=router;
